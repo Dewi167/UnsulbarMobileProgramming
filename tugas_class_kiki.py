6c5efb13088101256b8424eb55545c377ed2590c
Dewi Nurliana <97575981+Dewi167@users.noreply.github.com>
2022-06-19 14:34:28 +0800

    Tugas Mobile

--- tugas_class_kiki.py
+++ tugas_class_kiki.py
@@ -1,11 +1,10 @@
-#kiki malinda_D0219340
 class mahasiswa():
     jurusan = "teknik informatika"
  
     def __init__(self, input_nama):
         self.nama = input_nama # public
  
-kiki = mahasiswa("kiki malinda")
+kiki = mahasiswa("Dewi Nurliana")
  
 print(mahasiswa.jurusan)
 print(kiki.jurusan)
